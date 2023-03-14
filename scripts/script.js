38527f615929e796904b003f4ec8645c3beef8ce
Yuval sandler <sandler2806@gmai.com>
2023-03-14 21:09:33 +0200

    final popup alert

--- scripts/script.js
+++ scripts/script.js
@@ -7,21 +7,11 @@ domains_demo=["www.maccabi4u.co.il"]
 let domain=window.location.hostname;
 last_visit=localStorage.getItem("last_visit")
 console.log(last_visit)
-if(last_visit === null || time-last_visit>5){
+if(last_visit === null || time-last_visit>8){
     if(domains_demo.includes(domain)){
-        if(confirm("היי ישנה בקשה פתוחה")){
-            // chrome.action.openPopup()
-            chrome.runtime.sendMessage({ message: "open_popup" });
-            console.log("you are in")
-            chrome.browserAction.openPopup();
-        }
+        alert("שלום, ישנם מדריכים קיימים לאתר זה בתוסף")
     }
 }
 localStorage.setItem("last_visit",time)
 last_visit=localStorage.getItem("last_visit")
 console.log(last_visit)
-
-// document.getElementById("open-popup-button").addEventListener("click", function() {
-//     chrome.runtime.sendMessage({ message: "open_popup" });
-//   });
-  
