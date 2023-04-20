089c73e29f9443b30a08a7a4b8ca4153f80af97e
noamv2 <74304423+noamv2@users.noreply.github.com>
2023-04-20 13:51:29 +0300

    added server api for the popup

--- scripts/server_api.js
+++ scripts/server_api.js
@@ -1,20 +1,41 @@
 server_ip = '127.0.0.1'
 
+
 function get_guide(domain_name, guide_name) {
+    const url = `http://c/guide/${domain_name}/${guide_name}`;
+    const http = new XMLHttpRequest();
+    
     return new Promise((resolve, reject) => {
-        const guide_url = `https://${server_ip}/guide/${domain_name}/${guide_name}`;
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', guide_url);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    const data = JSON.parse(xhr.responseText);
-                    resolve(data); 
-                } else {
-                    reject(new Error('Request failed'));
-                }
-            }
-        };
-        xhr.send();
+      http.onreadystatechange = () => {
+        if (http.readyState === XMLHttpRequest.DONE) {
+          if (http.status === 200) {
+            resolve(http.responseText);
+          } else {
+            reject(new Error('Request failed'));
+          }
+        }
+      };
+    
+      http.open('GET', url);
+      http.send();
     });
-}
+  }
+  
+  function post_guide(guide)
+  {
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", `http://${server_ip}:5000/guide`);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+      }};
+    
+
+
+    xhr.send(guide);
+  }
