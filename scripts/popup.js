7beddaa7321236ecad7d3843c1d69ff78a332c81
noamv2 <74304423+noamv2@users.noreply.github.com>
2023-05-17 12:17:03 +0300

    added server methods to popup.js

--- scripts/popup.js
+++ scripts/popup.js
@@ -17,6 +17,49 @@ supported_domains = {
     ]
   }
 
+  server_ip = '127.0.0.1'
+
+
+  function get_guide(domain_name, guide_name) {
+      const url = `http://${server_ip}:5000/guide/${domain_name}/${guide_name}`;
+      const http = new XMLHttpRequest();
+      
+      return new Promise((resolve, reject) => {
+        http.onreadystatechange = () => {
+          if (http.readyState === XMLHttpRequest.DONE) {
+            if (http.status === 200) {
+              resolve(http.responseText);
+            } else {
+              reject(new Error('Request failed'));
+            }
+          }
+        };
+      
+        http.open('GET', url);
+        http.send();
+      });
+    }
+    
+    function post_guide(guide)
+    {
+  
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", `http://${server_ip}:5000/guide`);
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.setRequestHeader("Content-Type", "application/json");
+      
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          console.log(xhr.status);
+          console.log(xhr.responseText);
+        }};
+      
+  
+  
+      xhr.send(guide);
+    }
+
+
 async function get_current_domain()
 {
 
