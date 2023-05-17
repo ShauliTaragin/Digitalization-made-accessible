79e518e8ab70b6b768992c28321e5f286ed859ce
ShauliTaragin <shaulitaragin@gmail.com>
2023-05-17 15:51:06 +0300

    Able to navigate to URL but unalbe to initiate guide currently

--- scripts/ViewGuides.js
+++ scripts/ViewGuides.js
@@ -33,3 +33,35 @@ const supported_domains = {
   }
   
   add_guides_to_drop_down(supported_domains);
+
+
+  function getURLFromGuideName(guideName) {
+    for (const domain of supported_domains.domains) {
+      const guideIndex = domain.guides.indexOf(guideName);
+      if (guideIndex !== -1) {
+        return domain.name;
+      }
+    }
+    return null; // Guide name not found
+  }
+  const button = document.getElementById("Go-to-chosen-option-button");
+
+  // Add a click event listener to the button
+  button.addEventListener("click", go_to_guide_after_click);
+
+  // button.addEventListener("click", async function () {
+  function go_to_guide_after_click(){
+    console.log("here");
+    
+    const dropdown = document.getElementById("guide-list");
+    const a = dropdown.value
+  //   //get URL from dropdown
+  //   const url = getURLFromGuideName(dropdown.value);
+    console.log("a is " + a);
+    // Navigate to the URL
+    const url = "https://www.maccabi4u.co.il";
+    chrome.tabs.create({ url });
+    sleep(5000); 
+    chrome.runtime.sendMessage({ greeting: a });
+  // })
+}
