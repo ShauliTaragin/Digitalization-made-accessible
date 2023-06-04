bd4bd53656aa703b3c9791e5fd6012aa74aa223a
Yuval sandler <sandler2806@gmai.com>
2023-06-04 13:44:42 +0300

    add bool for one use only and remove color after click

--- scripts/content.js
+++ scripts/content.js
@@ -2,10 +2,10 @@
 async function waitForClick(btn, guide) {
   return new Promise((resolve, reject) => {
     btn.addEventListener("click", function() {
-      //btn.removeEventListener('click', function() {})
       guide.shift()
       console.log(guide)
       chrome.storage.local.set({ 'active_guide': guide }, function() {});
+      btn.style.backgroundColor = "";
       resolve();
     });
   });
@@ -36,11 +36,16 @@ async function waitForClick(btn, guide) {
     element.style.backgroundColor = "yellow";
     element.scrollIntoView({ behavior: 'smooth', block: 'center' });
   }
+  function step_found(guide){
+    guide[0].found=true
+    chrome.storage.local.set({ 'active_guide': guide }, function() {});
+  }
 
   async function execute_step(guide_step,guide)
   {
     console.log(guide_step)
     let element = await get_element(guide_step['key'], guide_step['value'])
+    step_found(guide)
     console.log(element)
     color_element(element)
     await waitForClick(element,guide)
@@ -51,9 +56,9 @@ async function waitForClick(btn, guide) {
     chrome.storage.local.get(['active_guide'], (result) =>
     { 
       const steps = result['active_guide']
-      console.log(steps)
-      if(result != undefined)
+      if(steps.length>0 && !steps[0].found)
       {
+        console.log(steps)
         execute_step(steps[0], steps)  
       }
 
