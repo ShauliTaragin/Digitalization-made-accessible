b503843f9ea1461bd16fd7bc461dc78dc40ec1d7
Yuval sandler <sandler2806@gmai.com>
2023-07-21 11:12:22 +0300

    some cleaning working

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,23 +1,4 @@
 
-supported_domains = {
-    "domains": [
-      {
-        "name": "www.btl.gov.il",
-        "guides": [
-          "טופס 3010",
-          "קצבת נכות"
-        ]
-      },
-      {
-        "name": "www.maccabi4u.co.il",
-        "guides": [
-          "קביעת תור לרופא",
-          "הזמנת רופא לבית"
-        ]
-      }
-    ]
-  }
-
   async function get_guide(domain_name, guide_name) {
       const url = `https://flask-server-deplohy.herokuapp.com/guide/${domain_name}/${guide_name}`;
       const http = new XMLHttpRequest();
@@ -57,46 +38,8 @@ supported_domains = {
         http.send();
       });
     }
-
-    function getAllDomains() {
-      const url = 'https://flask-server-deplohy.herokuapp.com/domains';
-      const http = new XMLHttpRequest();
     
-      return new Promise((resolve, reject) => {
-        http.onreadystatechange = () => {
-          if (http.readyState === XMLHttpRequest.DONE) {
-            if (http.status === 200) {
-              resolve(JSON.parse(http.responseText));
-            } else {
-              reject(new Error('Request failed'));
-            }
-          }
-        };
-    
-        http.open('GET', url);
-        http.send();
-      });
-    }
-    
-    
-    function post_guide(guide)
-    {
   
-      let xhr = new XMLHttpRequest();
-      xhr.open("POST", `http://${server_ip}:5000/guide`);
-      xhr.setRequestHeader("Accept", "application/json");
-      xhr.setRequestHeader("Content-Type", "application/json");
-      
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          console.log(xhr.status);
-          console.log(xhr.responseText);
-        }};
-      
-  
-  
-      xhr.send(guide);
-    }
 
 
 async function get_current_domain()
@@ -125,20 +68,6 @@ function add_guides_to_drop_down(guides)
       
       dropdown.appendChild(option)
     }
-}
-
-function get_domain_guides(domain_name)
-{
-    for(let i = 0; i < supported_domains.domains.length; ++i)
-    {
-        const current_domain = supported_domains.domains[i]
-        if(current_domain.name == domain_name)
-        {
-            return current_domain.guides;
-        }
-    }
-
-    return []
 }
 
 
@@ -175,6 +104,3 @@ notification.addEventListener("click", async function () {
   }
   )
 })
-
-
-// export  {getAllDomains, getGuidesByDomain, get_guide} ;
